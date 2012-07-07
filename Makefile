4150541cabcbb40ab0d8368c501e7906a2c601d9
Cory Martin <bluplastik@gmail.com>
2012-07-06 20:27:25 -0400

    Reorganized tests directory layout

--- Makefile
+++ Makefile
@@ -3,11 +3,13 @@ compile-tests:
 	@./node_modules/coffee-script/bin/coffee \
 		--compile \
 		--bare \
-		./test/*.coffee
+		--output ./test/ \
+		./test/src/
 
 test-run:
 	@./node_modules/.bin/mocha \
 		--require expect.js \
+		--ui bdd \
 		--reporter dot
 
 test: compile-tests test-run
--- test/list.js
+++ test/list.js
@@ -6,6 +6,48 @@ expect = require('expect.js');
 List = require('../lib/coll').List;
 
 describe('List', function() {
+  describe('List Constructor', function() {
+    var ls1, ls2, ls3, ls4;
+    ls1 = ls2 = ls3 = ls4 = null;
+    beforeEach(function() {
+      ls1 = new List;
+      ls2 = new List(['a', 2]);
+      ls3 = List();
+      return ls4 = List([1, 'z']);
+    });
+    it('should instantiate a new instance with or without the use of `new`', function() {
+      expect(ls1).to.be.a(List);
+      expect(ls2).to.be.a(List);
+      expect(ls3).to.be.a(List);
+      return expect(ls4).to.be.a(List);
+    });
+    it('should set the inital length of the list', function() {
+      expect(ls1.length).to.be(0);
+      expect(ls2.length).to.be(2);
+      expect(ls3.length).to.be(0);
+      return expect(ls4.length).to.be(2);
+    });
+    it('should populate itself with some initial values via the init param', function() {
+      expect(ls2[0]).to.be('a');
+      expect(ls2[1]).to.be(2);
+      expect(ls4[0]).to.be(1);
+      return expect(ls4[1]).to.be('z');
+    });
+    return it('should accept an "iterable" for the init parameter', function() {
+      var foo, ls;
+      ls = List([1, 2]);
+      expect(ls.length).to.be(2);
+      ls = List('as');
+      expect(ls.length).to.be(2);
+      ls = List(List('as'));
+      expect(ls.length).to.be(2);
+      foo = function() {
+        ls = List(arguments);
+        return expect(ls.length).to.be(2);
+      };
+      return foo('a', 'b');
+    });
+  });
   describe('Instance Properties', function() {
     return describe('length', function() {
       var ls;
